861684cb649cb9943317c9b0927d462915ae0afa
icbicket <icbicket@users.noreply.github.com>
2016-09-10 20:58:12 -0400

    Organized code and made Image, ImageAligner, AlignPlot classes: Model View Controller!

--- AligningTest.py
+++ AligningTest.py
@@ -1,8 +1,11 @@
 import AlignPair
-import numpy as np
+import Image
+import ImageAligner
 from skimage import data
 
-p1 = data.camera()
-p2 = data.coins()
-Ap = AlignPair.AlignPair(p1, p2)
+p1 = Image.Image(data.camera())
+p2 = Image.Image(data.coins())
+p3 = Image.Image(data.chelsea()[:, :, 0])
+p4 = Image.Image(data.astronaut()[:, :, 0])
+Ap = ImageAligner.ImageAligner((p1, p2, p3, p4))
 plotter = AlignPair.AlignPlot(Ap)
---
+++ Image.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Image(object):
+	def __init__(self, Img):
+		"""Function to initialize image class: Img = 2d numpy array"""
+		self.Img = Img
+		self.size = np.shape(Img)
+		#Extract the contrast limits for the input image (min and max intensity value)
+		self.Imglim = [np.min(self.Img[~np.isnan(self.Img)]), np.max(self.Img[~np.isnan(self.Img)])]
+
+	def PadImg(self, pad):
+		#Pad image, input pad = 2x2 array/tuple ((axis0_before, axis0_after), (axis1_before, axis1_after))
+		self.Img = np.pad(self.Img.astype(float), pad, 'constant', constant_values = (np.nan,))
