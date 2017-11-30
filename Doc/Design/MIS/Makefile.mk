9dd27c750e09e9a36704b1954ad192479e69c1b3
icbicket <icbicket@users.noreply.github.com>
2017-11-29 22:28:16 -0500

    Adding makefile to MIS doc

---
+++ Doc/Design/MIS/Makefile.mk
@@ -0,0 +1,38 @@
+LATEX=pdflatex
+LATEXOPT=--shell-escape
+NONSTOP=--interaction=nonstopmode
+
+LATEXMK=latexmk
+LATEXMKOPT=-pdf
+
+
+MAIN=MIS
+SOURCES=$(MAIN).tex Makefile ../../SRS/SRS.tex ../MG/ModuleHierarchyTable.tex
+
+all:    $(MAIN).pdf
+
+.refresh:
+	touch .refresh
+
+$(MAIN).pdf: $(MAIN).tex .refresh $(SOURCES)
+		$(LATEXMK) $(LATEXMKOPT) $(CONTINUOUS) \
+			-pdflatex="$(LATEX) $(LATEXOPT) $(NONSTOP) %O %S" $(MAIN)
+
+force:
+		touch .refresh
+		$(LATEXMK) $(LATEXMKOPT) $(CONTINUOUS) \
+			-pdflatex="$(LATEX) $(LATEXOPT) %O %S" $(MAIN)
+
+clean:
+		$(LATEXMK) -C $(MAIN)
+		rm -f $(MAIN).pdfsync
+		rm -rf *~ *.tmp
+		rm -f *.bbl *.blg *.aux *.end *.fls *.log *.out *.fdb_latexmk
+
+once:
+		$(LATEXMK) $(LATEXMKOPT) -pdflatex="$(LATEX) $(LATEXOPT) %O %S" $(MAIN)
+
+debug:
+		$(LATEX) $(LATEXOPT) $(MAIN)
+
+.PHONY: clean force once all
