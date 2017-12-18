9989a0f71c09be21bdebb5a64f739f43ec4d41b3
icbicket <icbicket@users.noreply.github.com>
2017-12-17 21:52:23 -0500

    Updates to Test Report and Dependencies file

--- src/DeconvolutionBlankError.py
+++ src/DeconvolutionBlankError.py
@@ -5,7 +5,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import collections
-import RMS
+import rms
 
 folder = 'TestCase'
 
@@ -16,14 +16,14 @@ spec1plot = SpectrumPlotter.SpectrumManager(spec1)
 blankPSF = np.zeros(np.size(spec1.intensity))
 blankPSF[int(len(blankPSF)/2)] = 1
 blankPSF = Spectrum.EELSSpectrum(blankPSF, ZLP=True, dispersion=spec1.dispersion)
-print(blankPSF.ZLP)
+
 iterations = np.array([0, 1, 5, 10, 50, 100, 500, 1000, 5000])
 spec1RL = collections.OrderedDict()
 error = []
 for ii in iterations:
-	spec1RL[ii] = spec1.RLDeconvolution(ii, blankPSF, PSF_pad=None)
+	spec1RL[ii] = spec1.RLDeconvolution(ii, blankPSF)
 	spec1plot.update_spectrum(spec1RL[ii], ID='RL' + str(ii))
-	error.append(RMS.RMS(spec1RL[ii].intensity, spec1.intensity))
+	error.append(rms.rms(spec1RL[ii].intensity, spec1.intensity))
 spec1plot.add_legend()
 
 fig_error = plt.figure('RMS error')
