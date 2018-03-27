b5ad674d2ec7ee4f27e58fae701f220b2c1c88ee
icbicket <icbicket@users.noreply.github.com>
2018-03-27 15:46:27 -0400

    Added constants to repo

---
+++ constants.py
@@ -0,0 +1,5 @@
+BOLTZMANN = 1.38064852e-23 # Joules per Kelvin
+COULOMB = 1.6021766208e-19 # Joules per electron volt
+ELECTRONMASS = 9.10938356e-31 # kilograms
+LIGHTSPEED = 299792458 # meters per second
+PLANCK = 6.626070040e-34 #Joule-seconds
